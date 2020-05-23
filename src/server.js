d7f2a75a28be6cea891922a8b1c30dc299cd9c1f
Francisco.Daines <francisco.daines@latam.com>
2020-05-23 13:04:40 -0400

    Adding POST operation to server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   res.send({ name: "Guest" })
+})
+
+app.post("/", (req, res) => {
+  const formData = {
+    name: req.body.name,
+    age: req.body.age
+  }
+
+  res.send(formData)
 })
 
 module.exports = app
--- test/server.test.js
+++ test/server.test.js
@@ -1,9 +1,25 @@
 const supertest = require("supertest");
 const app = require("../src/server")
 
-test("GET /", done => {
-  supertest(app)
-    .get("/")
-    .expect(200, JSON.stringify({ name: "Guest" }))
-    .end(done)
-})
+describe("Testing server.js operations ", () => {
+  it("GET /", done => {
+    supertest(app)
+      .get("/")
+      .expect(200, JSON.stringify({ name: "Guest" }))
+      .end(done)
+  })
+
+  it("POST /", async done => {
+    return supertest(app)
+      .post("/")
+      .send({
+         'name': 'Mike Wazowski',
+         'age': '98'
+       })
+      .expect(response => {
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual({ name: "Mike Wazowski", age: "98" })
+        done()
+      })
+  })
+});
